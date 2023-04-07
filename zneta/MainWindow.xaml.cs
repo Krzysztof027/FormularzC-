7174fb073a180f2c268bbe077080d79e37b7460a
Krzyszof027 <krzysztofszczerkowski1492@gmail.com>
2023-04-08 00:18:32 +0200

    Formularz cz1

---
+++ zneta/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication7
+{
+    /// <summary>
+    /// MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isFirstRun;
+
+        public MainWindow()
+        {
+            isFirstRun = true;
+
+            InitializeComponent();
+
+            isFirstRun = false;
+        }
+
+        private void Odp_A_checked(object sender, RoutedEventArgs e)
+        {
+            if (isFirstRun)
+                return;
+            App.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void Odp_B_Checked(object sender, RoutedEventArgs e)
+        {
+            App.Background = new SolidColorBrush(Colors.Red);
+        }
+        private void Odp_C_Checked(object sender, RoutedEventArgs e)
+        {
+            App.Background = new SolidColorBrush(Colors.Green);
+        }
+
+        private void Sprawdz(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
